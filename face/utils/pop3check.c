fea80a78b92e0c4a35a228ca3e922caa1d1fd731
erik <erik>
2001-01-14 02:19:18 +0000

    created pop3check util face

---
+++ face/utils/pop3check.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "defs.h"
+#include "db.h"
+
+#ifndef _
+#define _(X) (X)
+#endif
+
+int
+face_uses_X ()
+{
+  return GEMS_FALSE;
+}
+
+void
+oops (char *a, char *b)
+{
+  printf ("%s: %s\n", a, b);
+}
+
+int
+face_run (int argc, char **margv)
+{
+  printf ("\n");
+  return GEMS_TRUE;
+}
+

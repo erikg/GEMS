f2cdab6c9e015a2cb770b0b4f6f7d8502aab5636
erik <erik>
2001-01-25 23:02:30 +0000

    *** empty log message ***

--- face/GNOME/o/callbacks.c
+++ face/GNOME/o/callbacks.c
@@ -378,7 +378,44 @@ on_toolbar_compose_clicked (GtkButton * button, gpointer user_data)
 void
 on_toolbar_reply_clicked (GtkButton * button, gpointer user_data)
 {
+    synopsis *s;
+    GtkCTreeNode *n;
+    GtkWidget *from, *subj, *date, *recipt, *body, *comp;
+    char *shtuff;
 
+    if (widget != NULL)
+    {
+	n = gtk_ctree_node_nth (GTK_CTREE (widget),
+				(&(GTK_CTREE (widget)->clist))->focus_row);
+	s = (synopsis *) gtk_ctree_node_get_row_data (GTK_CTREE (widget), n);
+	shtuff = (char *) db_read_body (s->id);
+    } else
+	return;
+
+    comp = create_compose ();
+
+    from = (GtkWidget *) gtk_object_get_data (GTK_OBJECT (comp), "entry1");
+    date = (GtkWidget *) gtk_object_get_data (GTK_OBJECT (comp), "entry2");
+    subj = (GtkWidget *) gtk_object_get_data (GTK_OBJECT (comp), "entry3");
+    recipt = (GtkWidget *) gtk_object_get_data (GTK_OBJECT (comp), "entry4");
+    body = (GtkWidget *) gtk_object_get_data (GTK_OBJECT (comp), "text1");
+
+    gtk_entry_set_text (GTK_ENTRY (from), s->sender);
+    gtk_entry_set_text (GTK_ENTRY (subj), s->subject);
+    gtk_entry_set_text (GTK_ENTRY (date), s->date);
+    gtk_text_freeze (GTK_TEXT (body));
+    gtk_text_set_point (GTK_TEXT (body), 0);
+    gtk_text_forward_delete (GTK_TEXT (body),
+			     gtk_text_get_length (GTK_TEXT (body)));
+    gtk_text_insert (GTK_TEXT (body), NULL, NULL, NULL, shtuff, -1);
+    gtk_text_thaw (GTK_TEXT (body));
+    if (widget == NULL)
+    {
+	free (s);
+	free (shtuff);
+    }
+    gtk_widget_show (comp);
+    return;
 }
 
 

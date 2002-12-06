ea7aa754179e33675a16bcf56eaadeff17db415f
erik <erik>
2002-12-06 22:20:42 +0000

    regex fix

--- common/regex.c
+++ common/regex.c
@@ -8,15 +8,13 @@ int
 regex (char *r, char *s)
 {
     void *preg;
-    regmatch_t *pmatch;
     int val;
 
     preg = (void *) malloc (1000 * sizeof (void *));
-    pmatch = (regmatch_t *) malloc (1000 * sizeof (regmatch_t));
 
     regcomp (preg, r, 0);
 
-    if (regexec (preg, s, strlen (s), pmatch, 0) == 0)
+    if (regexec (preg, s, 0, NULL, 0) == 0)
 	val = GEMS_TRUE;
     else
 	val = GEMS_FALSE;

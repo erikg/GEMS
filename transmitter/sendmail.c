0daa41055472bb0e2cc618281dbc7b0bd0532ba6
erik <erik>
2003-04-05 18:29:45 +0000

    beginnings of sendmail transmission

--- transmitter/sendmail.c
+++ transmitter/sendmail.c
@@ -1,1 +1,30 @@
+
+/*
+ * $Id: sendmail.c,v 1.2 2003/04/05 18:29:45 erik Exp $
+ */
+
+#include <stdio.h>
 #include "transmitter.h"
+
+static char *cmd[] = { "/usr/sbin/sendmail", "-oi", "-t", NULL };
+
+int transmit_sendmail (message *)
+{
+	int pid;
+	pid = fork();
+	switch(pid)
+	{
+		case -1:
+			oops("Whoa, catastauphic failure","Fork failed!!!");
+			return -1;
+			break;
+		case 0:		/* child */
+			execve(cmd);
+			break;
+		default:	/* parent */	
+
+			waitpid(pid,NULL,0);
+			break;
+	}
+	return 0;
+}
--- transmitter/transmitter.c
+++ transmitter/transmitter.c
@@ -1,1 +1,9 @@
+#include "message.h"
 #include "transmitter.h"
+
+int transmit_sendmail (message *);
+
+int transmit_message (message * m, int agent)
+{
+	return transmit_sendmail (m);
+}

049dd048b62ce41320fd07644532828b47a12358
erik <erik>
2003-04-05 18:36:30 +0000

    added license header and Id tag

--- include/receptor.h
+++ include/receptor.h
@@ -1,3 +1,28 @@
+/*****************************************************************************
+ *    GEMS Email Client                                                      *
+ *                                                                           *
+ *     Copyright (C) 2000-2003 Erik Greenwald <erik@smluc.org>               *
+ *                                                                           *
+ *     This program is free software; you can redistribute it and/or modify  *
+ *     it under the terms of the GNU General Public License as published by  *
+ *     the Free Software Foundation; either version 2 of the License, or     *
+ *     (at your option) any later version.                                   *
+ *                                                                           *
+ *     This program is distributed in the hope that it will be useful,       *
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *     GNU General Public License for more details.                          *
+ *                                                                           *
+ *     You should have received a copy of the GNU General Public License     *
+ *     along with this program; if not, write to the Free Software           *
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
+ *                                                                           *
+ *****************************************************************************/
+
+/*
+ * $Id: receptor.h,v 1.4 2003/04/05 18:36:29 erik Exp $
+ */
+
 #ifndef __GEMS_RECEPTOR_H_
 #define __GEMS_RECEPTOR_H_
 
--- transmitter/sendmail.c
+++ transmitter/sendmail.c
@@ -1,6 +1,31 @@
+/*****************************************************************************
+ *    GEMS Email Client                                                      *
+ *                                                                           *
+ *     Copyright (C) 2000-2003 Erik Greenwald <erik@smluc.org>               *
+ *                                                                           *
+ *     This program is free software; you can redistribute it and/or modify  *
+ *     it under the terms of the GNU General Public License as published by  *
+ *     the Free Software Foundation; either version 2 of the License, or     *
+ *     (at your option) any later version.                                   *
+ *                                                                           *
+ *     This program is distributed in the hope that it will be useful,       *
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *     GNU General Public License for more details.                          *
+ *                                                                           *
+ *     You should have received a copy of the GNU General Public License     *
+ *     along with this program; if not, write to the Free Software           *
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
+ *                                                                           *
+ *****************************************************************************/
 
 /*
- * $Id: sendmail.c,v 1.2 2003/04/05 18:29:45 erik Exp $
+ * $Id: sendmail.c,v 1.3 2003/04/05 18:36:30 erik Exp $
+ */
+
+
+/*
+ * $Id: sendmail.c,v 1.3 2003/04/05 18:36:30 erik Exp $
  */
 
 #include <stdio.h>

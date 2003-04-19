8283396fd0d5439c4a3485cf29e83d4f21338aa3
erik <erik>
2003-04-19 13:44:14 +0000

    tweaks to insert loop

--- face/utils/cull.c
+++ face/utils/cull.c
@@ -20,22 +20,18 @@
  *****************************************************************************/
 
 /*
- * $Id: snarf.c,v 1.10 2003/04/19 13:44:14 erik Exp $
+ * $Id: cull.c,v 1.1 2003/04/19 13:44:14 erik Exp $
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include "defs.h"
+#include "spoolread.h"
+#include "ll.h"
 #include "message.h"
 #include "db.h"
-
-#define MEGS *1024*1024
-
-	/* 8 megs */
-#define BIGBUFSIZ (8 MEGS)
+#include "receptor.h"
 
 #ifndef _
 #define _(X) (X)
@@ -51,88 +47,45 @@ void
 oops (char *a, char *b)
 {
     printf ("%s: %s\n", a, b);
-    return;
 }
 
 int
-parse (char *buf, unsigned int size)
+face_run (int argc, char **margv)
 {
-    int i, linecount, linecountsize, msgcount, msgcountsize;
-    char **line, *walk, **msg, *mbox;
+    void *list;
+    message *mess;
+    char *spoolname, *mbox;
 
-    msgcount = linecount = 0;
-    msgcountsize = linecountsize = 1024;
-    line = malloc (sizeof (char *) * linecountsize);
-    msg = malloc (sizeof (char *) * msgcountsize);
-
-    if (!strncmp ("From ", buf, 5))
-	line[linecount++] = buf;
-    for (walk = buf; walk < (buf + size); walk++)
-	if (walk[0] == '\n')
-	{
-	    if (linecount >= linecountsize)
-		line = realloc (line, (linecountsize *= 2) * sizeof (char *));
-	    line[linecount++] = walk + 1;
-	}
-
-    for (i = 0; i < linecount; ++i)
-	if (!strncmp (line[i], "From ", 5))
-	    if (i == 0 || line[i - 1][0] == '\n')
-	    {
-		if (i > 0)
-		    line[i - 1][0] = 0;
-		msg[msgcount] = line[i];
-		msgcount++;
-	    }
-
-    i=0;
-    while (i < msgcount)
-	{
-		message *m;
-		m = message_build_from_buffer(msg[++i]);
-		if(m=NULL)
-			continue;
-		mbox = rule_check(m);
-		db_insert_msg(mbox,m);	/* this sucks up time */
-		message_destroy(m);
-		printf("\r                                                                               \r");
-		printf("%d/%d  (%.2f%%)", i, msgcount, 100.0*(float)i/(float)msgcount);
-		fflush(stdout);
-	}
-	printf("\n");
-    return 0;
-}
-
-int
-face_run (int argc, char **argv)
-{
-    int fd;
-    unsigned int size;
-    char *buf;
-
-    fd = open (argv[1], O_RDONLY);
-
-    if (fd <= 0)
-	return (-1);
+    /*
+     * if(argc!=2){printf("this face requires the name of a spool\n%s
+     * spool2sql -- <spool>\n\n",argv[0]);}
+     */
 
     if (rule_init () == GEMS_FALSE)
     {
-        printf (_("Failed to initialize ruleset\n"));
-        exit (EXIT_FAILURE);
+	printf (_("Failed to initialize ruleset\n"));
+	exit (EXIT_FAILURE);
     }
-
-    buf = (char *)malloc (BIGBUFSIZ);
-
-    size = read (fd, buf, BIGBUFSIZ);
-    buf[size] = 0;
-    parse (buf, size);
+    /*
+     * do shit. 
+     */
+    receive_pop3 ("freya", 110, "test", "tester");
 
     if (rule_close () == GEMS_FALSE)
     {
-        printf (_("Failed closing ruleset\n"));
+	printf (_("Failed closing ruleset\n"));
     }
+    /*
+     * close out the spool 
+     */
+    if (close_spool () != GEMS_TRUE)
+	printf (_("Couldn't close the spool\n"));
 
-    free (buf);
-    close (fd);
+    printf ("Normalizing\n");
+    if (db_normalize () == GEMS_FALSE)
+	printf (_("Failed to normalize the db"));
+
+    printf ("\n");
+
     return GEMS_TRUE;
 }

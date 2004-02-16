a54652f8dc77e4307d33eed37724a6b1d688350c
erik <erik>
2004-02-16 13:43:37 +0000

    oops stubs

--- db/PostGreSQL/db.pgsql.c
+++ db/PostGreSQL/db.pgsql.c
@@ -21,7 +21,7 @@
  *****************************************************************************/
 
 /*
- * $Id: db.pgsql.c,v 1.5 2004/02/14 19:41:28 erik Exp $
+ * $Id: db.pgsql.c,v 1.6 2004/02/16 13:43:37 erik Exp $
  */
 
 #include <libpq-fe.h>
@@ -32,21 +32,39 @@
 #include "rules.h"
 #include "face.h"		/* for oops() */
 
+struct table_s {
+    char *name, *vals;
+};
+
+struct table_s table[] = {
+    {"attachments", "id int, attachment text"},
+    {"body", "id int, body text"},
+    {"header", "id int, header text"},
+    {"mbox", "name text"},
+    {"prefs", "pref text, val text"},
+    {"recipt", "id int, recipt text"},
+    {"refs", "id int, childof int"},
+    {"replyto", "id int, replyto text"},
+    {"rules", "sort int, name text, regex text, mbox text, piece enum('Message','Body','Header','Subject','From','Recipients','Sender')"},
+    {"synopsis", "id int, mbox text, status set('read','marked') NOT NULL default '', senddate timestamp with timezone NOT NULL, sender text NOT NULL, subject text, charid text, PRIMARY KEY  (id)"},
+    NULL
+};
+
 PGconn *conn;
 
 /** initialize the db connection (done in shell) */
 int
 db_init (char *host, char *db, char *user, char *pass)
 {
-    char buf[4096];
+    char buf[BUFSIZ];
 
-    snprintf (buf, 4095, "host='%s' dbname='%s' user='%s' password='%s'", host,
+    snprintf (buf, BUFSIZ, "host='%s' dbname='%s' user='%s' password='%s'", host,
 	db, user, pass);
     conn = PQconnectdb (buf);
     if (PQstatus (conn) != CONNECTION_OK)
     {
 	oops ("Unable to connect to PostgreSQL database",
-	    PGerrorMessage (conn));
+	    (char *)PQerrorMessage (conn));
 	db_close (conn);
 	return GEMS_FALSE;
     }
@@ -56,26 +74,44 @@ db_init (char *host, char *db, char *user, char *pass)
 int
 db_init_firstrun ()
 {
+    struct table_s *t;
+    PGresult *res;
+    char buf[BUFSIZ];
+
+    t = table;
+    while (t && t->name && t->vals)
+    {
+	printf("%s\n\t%s\n\n", t->name, t->vals);
+	snprintf (buf, BUFSIZ, "create table \"%s\" (%s)", t->name, t->vals);
+	res = PQexec (conn, buf);
+	if (res == NULL)
+	    oops ("Whoa, critical error", "res was returned as null");
+	++t;
+    }
+    return GEMS_TRUE;
 }
 
 /** add a message to the db */
 int
 db_insert_msg (char *mbox, message * m)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return GEMS_FALSE;
 }
 
 /** add a mailbox to the db */
 int
 db_addmbox (char *mbox)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return GEMS_FALSE;
 }
 
 int
 db_addmbox_view (char *mbox, char *query)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return GEMS_FALSE;
 }
 
 /** drop a mailbox */
@@ -84,9 +120,12 @@ db_dropmbox (char *mbox)
 {
     char buf[BUFSIZ];
     PGresult *res;
-    snprintf(buf,BUFSIZ,"delete from mbox where name=%s", mbox);
-    res = PGexec(con, buf);
-    return;
+
+    snprintf (buf, BUFSIZ, "delete from mbox where name=%s", mbox);
+    res = PQexec (conn, buf);
+    if (res == NULL)
+	oops ("Whoa, critical error", "res was returned as null");
+    return GEMS_TRUE;
 }
 
 /** delete all entries in a table */
@@ -95,17 +134,20 @@ db_flush (char *table)
 {
     char buf[BUFSIZ];
     PGresult *res;
-    snprintf(buf,BUFSIZ,"delete from %s", table);
-    res = PQexec(con, buf);
-    return;
+
+    snprintf (buf, BUFSIZ, "delete from %s", table);
+    res = PQexec (conn, buf);
+    if (res == NULL)
+	oops ("Whoa, critical error", "res was returned as null");
+    return GEMS_TRUE;
 }
 
 /** shut down the db connection (done in shell) */
 int
 db_close ()
 {
-    PGfinish (conn);
-    return;
+    PQfinish (conn);
+    return GEMS_TRUE;
 }
 
 /** read the sender/subject/date/etc from a mailbox
@@ -113,27 +155,31 @@ db_close ()
 synopsis **
 db_read_synopsis (int mbox, int status)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return NULL;
 }
 
 synopsis **
 db_read_synopsis_raw (char *query)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return NULL;
 }
 
 /** read a list of all mailboxes on the system */
 mboxs **
 db_read_mboxlist (void)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return NULL;
 }
 
 /** read the body of an email for reading */
 char *
 db_read_body (int id)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return NULL;
 }
 
 /** read the entire message for forwarding, replying,
@@ -141,54 +187,62 @@ db_read_body (int id)
 message *
 db_read_message (int id)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return NULL;
 }
 
 /** make sure the tables are sane */
 int
 db_normalize ()
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return GEMS_FALSE;
 }
 
 /** returns 0 if no parent */
 int
 db_is_child_of (int msg)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return GEMS_FALSE;
 }
 
 /** return an array of rules */
 rule *
 db_fetch_rules (int *i)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return NULL;
 }
 
 /** set the rules (erasing the old ones)*/
 int
 db_set_rules (rule ** r)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return GEMS_FALSE;
 }
 
 /** set a preference */
 int
 db_pref_set (char *pref, char *val)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return GEMS_FALSE;
 }
 
 /** retrieve a preference */
 char *
 db_pref_get (char *pref)
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return NULL;
 }
 
 void
 db_syncharhash (void)
 {
+    oops("not implemented",__FUNCTION__);
     return;
 }
 
@@ -196,5 +250,6 @@ db_syncharhash (void)
 void
 db_purge_empty ()
 {
-    return;
+    oops("not implemented",__FUNCTION__);
+    return GEMS_FALSE;
 }

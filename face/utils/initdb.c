789237756853741d2cbb1145e1cdeb532d3ee815
erik <erik>
2005-01-06 02:33:57 +0000

    initialize database...

---
+++ face/utils/initdb.c
@@ -0,0 +1,53 @@
+
+/*****************************************************************************
+ *    GEMS Email Client                                                      *
+ *                                                                           *
+ *     Copyright (C) 2004-2005 Erik Greenwald <erik@smluc.org>               *
+ *                                                                           *
+ *     This program is free software; you can redistribute it and/or modify  *
+ *     it under the terms of the GNU General Public License as published by  *
+ *     the Free Software Foundation; either version 2 of the License, or     *
+ *     (at your option) any later version.                                   *
+ *                                                                           *
+ *     This program is distributed in the hope that it will be useful,       *
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *     GNU General Public License for more details.                          *
+ *                                                                           *
+ *     You should have received a copy of the GNU General Public License     *
+ *     along with this program; if not, write to the Free Software           *
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
+ *                                                                           *
+ *****************************************************************************/
+
+/*
+ * $Id: initdb.c,v 1.1 2005/01/06 02:33:57 erik Exp $
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "defs.h"
+#include "db.h"
+
+#ifndef _
+#define _(X) (X)
+#endif
+
+int
+face_uses_X ()
+{
+    return GEMS_FALSE;
+}
+
+void
+oops (char *a, char *b)
+{
+    printf ("%s: %s\n", a, b);
+}
+
+int
+face_run (int argc, char **margv)
+{
+    return GEMS_TRUE;
+}

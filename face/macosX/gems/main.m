4b83120afda0d627fcc178c7df7b1eb5655f4f92
erik <erik>
2009-09-08 20:18:30 +0000

    start an osX project

---
+++ face/macosX/gems/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  gems
+//
+//  Created by Erik Greenwald on 9/7/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
